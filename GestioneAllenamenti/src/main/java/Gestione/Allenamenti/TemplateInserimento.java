c15f6cda10d1108f5d3133a32a6b902accf0eaf8
PaoloM85 <paoloqwerty85@gmail.com>
2017-06-14 16:54:07 +0200

    merge main

--- GestioneAllenamenti/src/main/java/Gestione/Allenamenti/TemplateInserimento.java
+++ GestioneAllenamenti/src/main/java/Gestione/Allenamenti/TemplateInserimento.java
@@ -1,47 +1,46 @@
 package Gestione.Allenamenti;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
 public class TemplateInserimento {
 
 	public static void main(String[] args) {
+
+		/*
+		 * Connection conn = null; Statement stmt = null;
+		 * 
+		 * try {
+		 * 
+		 * Class.forName("com.mysql.jdbc.Driver"); Connection connection =
+		 * DriverManager.getConnection(
+		 * "jdbc:mysql://localhost/world?useSSL=false", "root", "root");
+		 */
+
+		ServicesCrud crud = new ServicesCrud("jpa-example");
+
+		PersonalTrainer newPersonalTrainer = new PersonalTrainer();
+		newPersonalTrainer.setNome(" luigia ");
+		newPersonalTrainer.setSettoreAllenamento(" calcio ");
+		crud.jpaCreate(newPersonalTrainer);
+		
+		PersonalTrainer newPersonalTrainer2 = new PersonalTrainer();
+		newPersonalTrainer2.setNome(" micias ");
+		newPersonalTrainer2.setSettoreAllenamento(" pallavolo ");
+		crud.jpaCreate(newPersonalTrainer2);
+		
+		PersonalTrainer newPersonalTrainer3 = new PersonalTrainer();
+		newPersonalTrainer3.setNome(" caldas");
+		newPersonalTrainer3.setSettoreAllenamento(" pallavolo ");
+		crud.jpaCreate(newPersonalTrainer3);
+		
+		PersonalTrainer newPersonalTrainer4 = new PersonalTrainer();
+		newPersonalTrainer4.setNome(" ugo");
+		newPersonalTrainer4.setSettoreAllenamento(" calcio ");
+		crud.jpaCreate(newPersonalTrainer4);
+		
+		PersonalTrainer newPersonalTrainer5 = new PersonalTrainer();
+		newPersonalTrainer5.setNome(" Lucaz");
+		newPersonalTrainer5.setSettoreAllenamento(" calcio ");
+		crud.jpaCreate(newPersonalTrainer5);
 		
 
-	/*	Connection conn = null;
-		Statement stmt = null;
-
-		try {
-
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/world?useSSL=false", "root",
-					"root");
-		*/
-	
-	ServicesCrud crud = new ServicesCrud("jpa-example");
-	
-	PersonalTrainer newPersonalTrainer = new PersonalTrainer();
-	newPersonalTrainer.setNome(" luigia ");
-	newPersonalTrainer.setSettoreAllenamento(" calcio ");
-	
-	PersonalTrainer newPersonalTrainer2 = new PersonalTrainer();
-	newPersonalTrainer.setNome(" micias ");
-	newPersonalTrainer.setSettoreAllenamento(" pallavolo ");
-	
-	PersonalTrainer newPersonalTrainer3 = new PersonalTrainer();
-	newPersonalTrainer3.setNome(" caldas");
-	newPersonalTrainer3.setSettoreAllenamento(" pallavolo ");
-	
-	PersonalTrainer newPersonalTrainer4 = new PersonalTrainer();
-	newPersonalTrainer4.setNome(" ugo");
-	newPersonalTrainer4.setSettoreAllenamento(" calcio ");
-	
-	PersonalTrainer newPersonalTrainer5 = new PersonalTrainer();
-	newPersonalTrainer5.setNome(" Lucaz");
-	newPersonalTrainer5.setSettoreAllenamento(" calcio ");
-	
-	crud.jpaCreate(newPersonalTrainer);
-	
-}
+	}
 }
